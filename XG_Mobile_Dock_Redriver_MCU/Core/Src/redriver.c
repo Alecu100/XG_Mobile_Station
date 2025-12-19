2c51b6fad8aee907b2ef007404028639aa0fa2e1
DESKTOP-Q7911QU\alecs <alecu89@yahoo.com>
2025-12-19 17:06:43 +0200

    more stability improvements

--- XG_Mobile_Dock_Redriver_MCU/Core/Src/redriver.c
+++ XG_Mobile_Dock_Redriver_MCU/Core/Src/redriver.c
@@ -275,10 +275,10 @@ static void initialize_cpu_gpu_redriver()
     initialize_channel_eq_profile(CPU_GPU_0_3_ADDR_I2C, CHANNEL_1_REGISTER, EQ_PROFILE_INDEX_13 | FLAT_GAIN_0 | FLAT_GAIN_1);
     initialize_channel_eq_profile(CPU_GPU_0_3_ADDR_I2C, CHANNEL_2_REGISTER, EQ_PROFILE_INDEX_13 | FLAT_GAIN_0 | FLAT_GAIN_1);
     initialize_channel_eq_profile(CPU_GPU_0_3_ADDR_I2C, CHANNEL_3_REGISTER, EQ_PROFILE_INDEX_13 | FLAT_GAIN_0 | FLAT_GAIN_1);
-    initialize_channel_eq_profile(CPU_GPU_4_7_ADDR_I2C, CHANNEL_4_REGISTER, EQ_PROFILE_INDEX_13 | FLAT_GAIN_0);
-    initialize_channel_eq_profile(CPU_GPU_4_7_ADDR_I2C, CHANNEL_5_REGISTER, EQ_PROFILE_INDEX_13 | FLAT_GAIN_0 | FLAT_GAIN_2);
-    initialize_channel_eq_profile(CPU_GPU_4_7_ADDR_I2C, CHANNEL_6_REGISTER, EQ_PROFILE_INDEX_13 | FLAT_GAIN_0 | FLAT_GAIN_2);
-    initialize_channel_eq_profile(CPU_GPU_4_7_ADDR_I2C, CHANNEL_7_REGISTER, EQ_PROFILE_INDEX_13 | FLAT_GAIN_0);
+    initialize_channel_eq_profile(CPU_GPU_4_7_ADDR_I2C, CHANNEL_4_REGISTER, EQ_PROFILE_INDEX_13 | FLAT_GAIN_0 | FLAT_GAIN_1);
+    initialize_channel_eq_profile(CPU_GPU_4_7_ADDR_I2C, CHANNEL_5_REGISTER, EQ_PROFILE_INDEX_13 | FLAT_GAIN_0 | FLAT_GAIN_1);
+    initialize_channel_eq_profile(CPU_GPU_4_7_ADDR_I2C, CHANNEL_6_REGISTER, EQ_PROFILE_INDEX_13 | FLAT_GAIN_0 | FLAT_GAIN_1);
+    initialize_channel_eq_profile(CPU_GPU_4_7_ADDR_I2C, CHANNEL_7_REGISTER, EQ_PROFILE_INDEX_13 | FLAT_GAIN_0 | FLAT_GAIN_1);
 }
 
 void power_up_down_redrivers(GPIO_PinState reset) {
